c2f253856995e6900901bc22a2118d4ce68b7b8a
allan-navarro <anavarro3106@gmail.com>
2020-10-18 13:03:12 -0600

    primer implementacion (lenta)

--- Parte 1/parte1_p1.m
+++ Parte 1/parte1_p1.m
@@ -3,5 +3,20 @@ pkg load image
 pkg load video
 imagen=imread('barbara.jpg');
 
-imagen_filtrada=mediana(imagen);
+[m,n,c]=size(imagen);
+
+%crea una matrix de 1 a m*n que representa el indice de cada pixel de 
+%la imagen de entrada
+tic
+pixel_index=reshape([1:m*n],n,[])';
+toc
+%aplica la funcion window a la matriz pixel_index
+[r,g,b]=arrayfun (@(o) window(imagen,o),pixel_index);
+imagen_filtrada=zeros(m,n,c);
+imagen_filtrada(:,:,1)=r;
+imagen_filtrada(:,:,2)=g;
+imagen_filtrada(:,:,3)=b;
+imagen_filtrada(1:5,1:5)
+imagen_filtrada=uint8(imagen_filtrada);
+
 imshow(imagen_filtrada)
---
+++ Parte 1/window.m
@@ -0,0 +1,22 @@
+%esta funcion calcula la mediana del pixel ubicado en la posicion "index"
+%llamar con  Res=arrayfun (@(o) window(A,o),[1 2 3;4 5 6;7 8 9]) ;
+%A = canal de entrada
+%w = mediana del pixel con una ventana de 3
+function [r,g,b] = window(A,index)
+  row = idivide(index,columns(A),'floor');
+  if row!= index/columns(A) 
+    row=row+1;
+  end
+  col = mod(index-1,columns(A))+1;
+  
+  start_col=max(1,col-1);
+  end_col= min(columns(A),col+1);
+  
+  start_row=max(1,row-1);
+  end_row=min(rows(A),row+1);
+  tic
+  r= median(median(A(start_row:end_row,start_col:end_col,1)));
+  g= median(median(A(start_row:end_row,start_col:end_col,2)));
+  b= median(median(A(start_row:end_row,start_col:end_col,3)));
+  toc
+endfunction
